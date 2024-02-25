8af6830b9805d7b2045f1ae2fdf1115facfeb8ae
bilaltoor <bilaltoor786786@gmail.com>
2024-02-25 01:49:30 -0800

    done

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,28 +1,38 @@
-import { getUrl } from "@/lib/utils";
-import { createClient } from "@/lib/supabase/serverClient";
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+import { type CookieOptions, createServerClient } from '@supabase/ssr'
 
 export async function GET(request: Request) {
-    // The `/auth/callback` route is required for the server-side auth flow implemented
-    // by the Auth Helpers package. It exchanges an auth code for the user's session.
-    // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
-    const requestUrl = new URL(request.url);
-    const code = requestUrl.searchParams.get("code");
-    const redirectPath = requestUrl.searchParams.get("redirect");
-    const redirect = getUrl() + redirectPath;
-
+    const { searchParams, origin } = new URL(request.url)
+    const code = searchParams.get('code')
+    // if "next" is in param, use it as the redirect URL
+    const next = searchParams.get('next') ?? '/'
+    console.log(origin, next, code)
     if (code) {
-        const cookieStore = cookies();
-        const supabase = createClient(cookieStore);
-
-        const { error } = await supabase.auth.exchangeCodeForSession(code);
+        const cookieStore = cookies()
+        const supabase = createServerClient(
+            process.env.NEXT_PUBLIC_SUPABASE_URL!,
+            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+            {
+                cookies: {
+                    get(name: string) {
+                        return cookieStore.get(name)?.value
+                    },
+                    set(name: string, value: string, options: CookieOptions) {
+                        cookieStore.set({ name, value, ...options })
+                    },
+                    remove(name: string, options: CookieOptions) {
+                        cookieStore.delete({ name, ...options })
+                    },
+                },
+            }
+        )
+        const { error } = await supabase.auth.exchangeCodeForSession(code)
         if (!error) {
-            // URL to redirect to after sign in process completes
-            return NextResponse.redirect(redirect ? redirect : requestUrl.origin);
+            return NextResponse.redirect(`https://gamify-store-rm8p4j26h-bilaltoor1.vercel.app${next}`)
         }
     }
 
     // return the user to an error page with instructions
-    return NextResponse.redirect(new URL("/auth/auth-code-error", request.url));
+    return NextResponse.redirect(`${origin}/auth/auth-code-error`)
 }
